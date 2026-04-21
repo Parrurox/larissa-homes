7345712197326dc05d452d35fdcdc1be18c959a7
parrurox <shaikhparwez963@gmail.com>
2026-04-21 12:42:48 +0530

    redirects fix

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,10 +1,14 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import LandingPage from "./pages/LandingPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: LandingPage,
+  },
+  {
+    path: "/investment",
+    element: <Navigate to="/investments" replace />,
   },
   {
     path: "/investments",
